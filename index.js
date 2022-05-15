51b9c6f03c2a13aaf87abf465fb12102d8185d96
genie <banggijin>
2022-05-16 01:06:06 +0900

    change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 const axios = require("axios");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
